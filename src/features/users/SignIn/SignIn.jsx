2406aa26d3f32611022e721b4b6374a4b82a5b81
VikrantSangwan <v.sangwan9876@gmail.com>
2023-03-06 12:12:31 +0530

    Folder Structure

---
+++ src/features/users/SignIn/SignIn.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./SignIn.css";
+function SignIn() {
+  return <h1>This is SignIn</h1>;
+}
+export default SignIn;
---
+++ src/features/users/SignUp/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./SignUp.css";
+
+function SignUp() {
+  return <h1>This is SignUp</h1>;
+}
+export default SignUp;
