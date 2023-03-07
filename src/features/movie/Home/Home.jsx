349134a0001f04bd5f2d4e67689d80cd06f286ef
VikrantSangwan <v.sangwan9876@gmail.com>
2023-03-07 12:22:07 +0530

    SignIn Page

---
+++ src/features/movie/Home/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+  return <h1>This is Home</h1>;
+}
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./features/movie/Home/Home";
+import SignIn from "./features/users/SignIn/SignIn";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
