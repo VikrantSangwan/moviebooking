67cf09365fc08ff754186d2ae81fe90fbb1b8f63
VikrantSangwan <v.sangwan9876@gmail.com>
2023-03-08 08:36:35 +0530

    Banner Completed

--- src/features/movie/Banner/Banner.jsx
+++ src/features/movie/Banner/Banner.jsx
@@ -1,11 +1,11 @@
-// Another Way of importing and using css
-import styles from "./Banner.css";
+import "./Banner.css";
+import React from "react";
 
 function Banner() {
   return (
-    <div className={styles.banner}>
-      <h1 className={styles.header1}>
-        GET <span className={styles.header2}>MOVIE</span> TICKETS
+    <div className="banner">
+      <h1 className="header1">
+        GET <span className="header2">MOVIE</span> TICKETS
       </h1>
       <p>Buy movie tickets in advance, find movie times watch trailers,</p>
       <p>read movie reviews and much more</p>
