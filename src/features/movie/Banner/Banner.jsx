88dd06f73f9ae5e882f4b5f75d3503c4402db5c2
VikrantSangwan <v.sangwan9876@gmail.com>
2023-03-20 01:37:59 +0530

    Banner Updated

--- src/features/movie/Banner/Banner.jsx
+++ src/features/movie/Banner/Banner.jsx
@@ -4,11 +4,14 @@ import React from "react";
 function Banner() {
   return (
     <div className="banner">
-      <h1 className="header1">
-        GET <span className="header2">MOVIE</span> TICKETS
-      </h1>
-      <p>Buy movie tickets in advance, find movie times watch trailers,</p>
-      <p>read movie reviews and much more</p>
+      <div className="bg-fixed-img"></div>
+      <div className="bannerContainer">
+        <h1 className="header1">
+          GET <span className="header2">MOVIE</span> TICKETS
+        </h1>
+        <p>Buy movie tickets in advance, find movie times watch trailers,</p>
+        <p>read movie reviews and much more</p>
+      </div>
     </div>
   );
 }
